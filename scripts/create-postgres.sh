f770b07d6b2c8a962002cf378f3bb3ef36388457
Glenn Plas <glenn@bitless.be>
2022-01-26 22:08:58 +0100

    update

--- scripts/create-postgres.sh
+++ scripts/create-postgres.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
-echo "Preparing Database ... $1 / $2 "
+echo "Installing postgres repositories ... $1 / $2 "
+
+sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+sudo yum -y install epel-release yum-utils
+sudo yum-config-manager --enable pgdg12
+
+echo "Installing postgres ... $1 / $2 "
+
+sudo yum install postgresql12-server postgresql12
+
+
+echo "Init database ... $1 / $2 "
+sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+
+echo "Enable startup service database ... $1 / $2 "
+sudo systemctl enable --now postgresql-12
+
+echo "Start database ... $1 / $2 "
+systemctl status postgresql-12
+
+exit 0
+
+echo "Preparing Database content ... $1 / $2 "
 
 DB=$1;
 USER=$2;
